f65c85479faafeed167b440889926b527da7e8dd
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-23 02:53:42 +0700

    Discarding app management admin

--- resources/views/backend/template.blade.php
+++ resources/views/backend/template.blade.php
@@ -111,7 +111,7 @@
                       <li><a href="{{ route('back.statistic.index') }}"><i class="fa fa-bar-chart "></i> Statistik API </a>
                   @elseif(strcasecmp(Auth::user()->role->name, 'Administrator')==0 )
                     <li><a href="{{ route('dashboard.administrator') }}"><i class="fa fa-desktop "></i> Aplikasi Internal </a>
-                      <li><a href="#"><i class="fa fa-users "></i> Pengelolaan Aplikasi Publik </a>
+                      <!-- <li><a href="#"><i class="fa fa-users "></i> Pengelolaan Aplikasi Publik </a> -->
                   @endif
                 </ul>
               </div>
