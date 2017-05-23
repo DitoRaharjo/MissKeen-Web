3a428155ea7e2f1666dc89274fb5dff2f3dd4362
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-24 01:17:10 +0700

    Fixing Search API

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -21,10 +21,65 @@ class SearchController extends Controller
 
       $ingredients = $input_data['ingredients'];
 
-      $recipes = Recipe::whereHas('ingredients.ingredientData', function($query) use($ingredients) {
-        $query->whereIn('id', $ingredients);
+      $jumlah = 0;
+      foreach ($ingredients as $hitung) {
+        $jumlah++;
+      }
+
+      $data = array();
+
+      $hasilSemua = Recipe::whereHas('ingredients', function ($query)  use ($ingredients) {
+        $query->where('ingredients.id', $ingredients[0]);
       })->get();
 
-      return response()->json($recipes);
+      if($jumlah == 1) {
+        $count = 0;
+        foreach ($hasilSemua as $hasil) {
+          $ingredientId = $hasil->ingredients->pluck('id');
+          foreach ($ingredientId as $id) {
+            foreach ($ingredients as $ingredient) {
+              if($id == $ingredient) {
+                $data[] = $hasil->id;
+              }
+            }
+          }
+          $count = 0;
+        }
+      } else if($jumlah > 1) {
+        $count = 0;
+        foreach ($hasilSemua as $hasil) {
+          $ingredientId = $hasil->ingredients->pluck('id');
+          foreach ($ingredientId as $id) {
+            foreach ($ingredients as $ingredient) {
+              if($id == $ingredient) {
+                // $data[] = $hasil->id;
+                $count++;
+              }
+              if($count > 1) {
+                if(!in_array($hasil->id, $data)) {
+                  $data[] = $hasil->id;
+                }
+              }
+            }
+          }
+          $count = 0;
+        }
       }
+
+      $recipes = array();
+
+      foreach ($data as $value) {
+        $recipes[] = Recipe::find($value);
+      }
+
+      if($recipes == null) {
+        $result = [
+          'status' => 'false',
+          'info' => 'there is no recipe with that ingredient yet',
+        ];
+        return response()->json($result);
+      } else {
+        return response()->json($recipes);
+      }
+    }
 }
