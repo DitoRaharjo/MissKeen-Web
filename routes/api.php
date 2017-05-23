f6e011487d9a3a0958de287c289427db314a958a
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-23 13:26:19 +0700

    fixing API Route

--- routes/api.php
+++ routes/api.php
@@ -18,38 +18,38 @@ use Illuminate\Http\Request;
 // });
 
 //------------------------------------------------ API-KEY ------------------------------------------------ \\
-// //For Testing API
-// Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogTest','checkApiKey']], function() {
-//   Route::get('test', 'API\TestController@urlQuery')->name('api.urlQuery');
-//   Route::post('test', 'API\TestController@postTest')->name('api.post');
-//   Route::put('test', 'API\TestController@putTest')->name('api.put');
-//   Route::patch('test', 'API\TestController@patchTest')->name('api.patch');
-//   Route::delete('test', 'API\TestController@deleteTest')->name('api.delete');
-// });
-//
-// //User Authentication and Registration
-// Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogUser','checkApiKey']], function() {
-//   Route::post('user/auth', 'API\AuthController@auth')->name('user.auth');
-//   Route::post('user/register', 'API\AuthController@register')->name('user.register');
-// });
-//
-// //Recipe Management
-// Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogRecipe','checkApiKey']], function() {
-//   Route::get('recipe', 'API\RecipeController@getAll')->name('recipe.getAll');
-//   Route::get('recipe/{id}', 'API\RecipeController@getOne')->name('recipe.getOne');
-//   Route::post('recipe', 'API\RecipeController@store')->name('recipe.store');
-//   Route::patch('recipe', 'API\RecipeController@update')->name('recipe.update');
-//   Route::delete('recipe', 'API\RecipeController@delete')->name('recipe.delete');
-// });
-//
-// //Ingredient Management
-// Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogIngredient','checkApiKey']], function() {
-//   Route::get('ingredient', 'API\IngredientController@getAll')->name('ingredient.getAll');
-//   Route::get('ingredient/{id}', 'API\IngredientController@getOne')->name('ingredient.getOne');
-//   Route::post('ingredient', 'API\IngredientController@store')->name('recipe.store');
-//   Route::patch('ingredient', 'API\IngredientController@update')->name('ingredient.update');
-//   Route::delete('ingredient', 'API\IngredientController@delete')->name('ingredient.delete');
-// });
+//For Testing API
+Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogTest','checkApiKey']], function() {
+  Route::get('test', 'API\TestController@urlQuery')->name('api.urlQuery');
+  Route::post('test', 'API\TestController@postTest')->name('api.post');
+  Route::put('test', 'API\TestController@putTest')->name('api.put');
+  Route::patch('test', 'API\TestController@patchTest')->name('api.patch');
+  Route::delete('test', 'API\TestController@deleteTest')->name('api.delete');
+});
+
+//User Authentication and Registration
+Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogUser','checkApiKey']], function() {
+  Route::post('user/auth', 'API\AuthController@auth')->name('user.auth');
+  Route::post('user/register', 'API\AuthController@register')->name('user.register');
+});
+
+//Recipe Management
+Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogRecipe','checkApiKey']], function() {
+  Route::get('recipe', 'API\RecipeController@getAll')->name('recipe.getAll');
+  Route::get('recipe/{id}', 'API\RecipeController@getOne')->name('recipe.getOne');
+  Route::post('recipe', 'API\RecipeController@store')->name('recipe.store');
+  Route::patch('recipe', 'API\RecipeController@update')->name('recipe.update');
+  Route::delete('recipe', 'API\RecipeController@delete')->name('recipe.delete');
+});
+
+//Ingredient Management
+Route::group(['prefix'=>env('API_VERSION'), 'middleware'=>['ApiLogIngredient','checkApiKey']], function() {
+  Route::get('ingredient', 'API\IngredientController@getAll')->name('ingredient.getAll');
+  Route::get('ingredient/{id}', 'API\IngredientController@getOne')->name('ingredient.getOne');
+  Route::post('ingredient', 'API\IngredientController@store')->name('recipe.store');
+  Route::patch('ingredient', 'API\IngredientController@update')->name('ingredient.update');
+  Route::delete('ingredient', 'API\IngredientController@delete')->name('ingredient.delete');
+});
 //------------------------------------------------ API-KEY ------------------------------------------------ \\
 
 //------------------------------------------------ WITHOUT API-KEY ------------------------------------------------ \\
