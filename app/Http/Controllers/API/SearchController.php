8630cbe64acf4a575ef03551e8c5b8171844d235
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-23 19:58:21 +0700

    Implement searching

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use DB;
+use Carbon\Carbon;
 
 use App\Recipe;
 use App\Ingredient;
+use App\IngredientData;
 
 class SearchController extends Controller
 {
     public function search(Request $request) {
       $input_data = $request->only('ingredient_id');
 
-      
+
     }
 
 
