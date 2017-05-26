bb13c75422ad9b62ffed4c67c03c5cd7e04b846b
Dito Raharjo <ditoraharjo@gmail.com>
2017-05-26 12:48:33 +0700

    Deleting android upload

--- dbDetails.php
+++
@@ -1,5 +0,0 @@
-define('HOST','139.162.50.79');
-define('USER','ditoraha_pwl');
-define('PASS','vincentius161');
-define('DB','ditoraha_misskeen');
-$con = mysqli_connect(HOST,USER,PASS,DB) or die('unable to connect to db');
